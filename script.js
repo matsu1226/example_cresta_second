bb13f031796afce50dd3d11b37f7eb262eb08e44
matsu1226 <81005900+matsu1226@users.noreply.github.com>
2021-04-05 16:23:53 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+$(function(){
+    let $win = $(window);
+    let $hbgToggle = function(){
+        $('.hbg-btn').toggleClass('close');
+        $('.header-nav').fadeToggle(500);
+        $('body').toggleClass('noscroll');
+    }
+
+    $('.hbg-btn').click($hbgToggle);
+
+    if($win.width() < 768){
+    $('.nav-item a[href]').click($hbgToggle);
+    }else{    
+        // 何もしない
+    };
+
+
+    // 追尾ヘッダー
+    let hvHeight = $('.fv').outerHeight(); 
+
+    $win.on('load scroll',function(){
+        let nowHeight = $(this).scrollTop();
+        if($win.width() > 768){
+            if(nowHeight > hvHeight){
+                $('header').addClass('fixed');
+            }else{
+                $('header').removeClass('fixed');
+            };
+        };
+    });
+
+});
+
+    //スライダー 
+    $('.slider').slick({
+    });
